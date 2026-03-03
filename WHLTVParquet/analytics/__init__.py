30b94ef55ba2d3b71ab088c5086561cee4789828
willholt1 <will.holt.xyz@gmail.com>
2026-03-03 20:38:54 +0000

    delete redundant code - move to use awpy fork

--- WHLTVParquet/analytics/__init__.py
+++
@@ -1,4 +0,0 @@
-# hltvparquet/analytics/__init__.py
-from . import players, rounds, kills, economy, utility, positions
-
-__all__ = ["players", "rounds", "kills", "economy", "utility", "positions"]
--- WHLTVParquet/loaders.py
+++
@@ -1,8 +0,0 @@
-# hltvparquet/loaders.py
-from .map import HLTVMap
-
-def load_map(parquet_path: str) -> HLTVMap:
-    """
-    Convenience loader for a single parquet map file.
-    """
-    return HLTVMap(parquet_path)
