c6fe311d58f837ba2e45cd35f702b6ba1f43552e
willholt1 <56679308+willholt1@users.noreply.github.com>
2026-04-26 18:27:03 +0100

    take inputs for patch and map name

--- DemoParser/validateParquet.py
+++ DemoParser/validateParquet.py
@@ -194,17 +194,21 @@ def main():
     parser = argparse.ArgumentParser(description="Verify Parquet vs Demo output.")
     parser.add_argument('--demo_file', type=str, help='Path to demo file')
     parser.add_argument('--parquet_file', type=str, help='Path to parquet file')
+    parser.add_argument('--patch_version', type=str, help='Patch version to use for ParquetDemo')
+    parser.add_argument('--map_name', type=str, help='Map name to use for ParquetDemo')
     parser.add_argument('--debug', action='store_true', help='Enable debug mode')
     args = parser.parse_args()
 
     debug = args.debug
     demo_file = args.demo_file
+    map_name = args.map_name
     parquet_file = args.parquet_file
+    patch_version = args.patch_version
     
     demo = Demo(demo_file, verbose=debug)
     demo.parse()
 
-    parquet_demo = ParquetDemo(parquet_file, map_name="de_overpass", patch_version="13962", verbose=debug)
+    parquet_demo = ParquetDemo(parquet_file, map_name=map_name, patch_version=patch_version, verbose=debug)
     parquet_demo.parse()
 
     validate_infernos(demo, parquet_demo)
